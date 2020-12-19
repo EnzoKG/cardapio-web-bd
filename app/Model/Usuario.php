93185f637d2818bdb4289d83ef6f66c18b6a46c5
EnzoKG <enzogiovanini@gmail.com>
2020-12-18 23:59:33 -0400

    adicionando getters e setters.

--- app/Model/Usuario.php
+++ app/Model/Usuario.php
@@ -17,4 +17,20 @@ class Usuario
     {
         return '@'.$this->login;
     }
+
+    public function setLogin($login) {
+        $this->login = $login;
+    }
+
+    public function getLogin() {
+        return $this->login;
+    }
+
+    public function setSenha($senha) {
+        $this->senha = $senha;
+    }
+
+    public function getSenha() {
+        return $this->senha;
+    }   
 }
