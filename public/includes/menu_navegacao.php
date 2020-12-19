583467812d01c000fd2da08ba259b9ff3b913dc7
EnzoKG <enzogiovanini@gmail.com>
2020-12-18 23:59:18 -0400

    adicionando botao registrar.

--- public/includes/menu_navegacao.php
+++ public/includes/menu_navegacao.php
@@ -1,15 +1,20 @@
 <?php
   session_start();
   $hrefLogin = '/public/pages/admin/form_login.php';
+  $hrefRegistro = '/public/pages/admin/form_registro.php';
   $isLoggedIn = "";
+  $registrar = "Registrar";
   $buttonLogin = "";
+  $buttonRegister = "";
   if(isset($_SESSION['logado'])) {
+    $buttonRegister = "";
     $hrefLogin = '/public/pages/admin/deslogar.php';
     $isLoggedIn = "Deslogar";
     $buttonLogin = '<button class="btn btn-outline-danger"><a href="'.$hrefLogin.'" class="navbar-text" style="text-decoration:none;">'.$isLoggedIn.'</a></button>';
   } else {
     $isLoggedIn = "Logar";
     $buttonLogin = '<button class="btn btn-outline-success"><a href="'.$hrefLogin.'" class="navbar-text" style="text-decoration:none;">'.$isLoggedIn.'</a></button>';
+    $buttonRegister = '<button class="btn btn-outline-primary" style="margin-right:1rem;"><a href="'.$hrefRegistro.'" class="navbar-text" style="text-decoration:none;">'.$registrar.'</a></button>';
   }
 ?>
 
@@ -18,4 +23,5 @@
     <a class="navbar-brand" href="#">Cardapio Web</a>
     <?=$buttonLogin?>
   </div>
+  <?=$buttonRegister?>
 </nav>
